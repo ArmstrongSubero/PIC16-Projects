0ad35ad4e3d315f22214bdf183fd89e7c49d82b3
Armstrong Subero <armstrongsubero@gmail.com>
2017-05-15 12:53:46 -0400

    Add files via upload

---
+++ I08_Joystick.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=I08_Joystick.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/I08_Joystick.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=i08joystick.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/i08joystick.x.tar
