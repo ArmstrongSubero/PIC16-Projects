efcbe982667db803b0b1c669b1429c97e1c6a542
Armstrong Subero <armstrongsubero@gmail.com>
2017-05-15 13:00:19 -0400

    Add files via upload

---
+++ I09_TTS.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=I09_TTS.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/I09_TTS.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=i09tts.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/i09tts.x.tar
