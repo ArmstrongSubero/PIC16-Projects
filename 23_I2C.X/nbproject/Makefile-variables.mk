20a9d04c0d4fb2a7629fa9d8a6cbfe1a82b33148
Armstrong Subero <armstrongsubero@gmail.com>
2017-05-15 12:20:27 -0400

    Add files via upload

---
+++ 23_I2C.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=23_I2C.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/23_I2C.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=23i2c.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/23i2c.x.tar
