c41b4db030009e5736423ce17ac5851efc6eae19
Armstrong Subero <armstrongsubero@gmail.com>
2017-05-15 13:11:16 -0400

    Add files via upload

---
+++ I10_RGB_LED.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=I10_RGB_LED.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/I10_RGB_LED.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=i10rgbled.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/i10rgbled.x.tar
