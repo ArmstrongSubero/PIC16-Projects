8a713e13525ce876c8879ce6893e4b5a4c622778
Armstrong Subero <armstrongsubero@gmail.com>
2017-05-15 20:05:13 -0400

    Add files via upload

---
+++ I19_Bluetooth_HC05.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=I19_Bluetooth_HC05.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/I19_Bluetooth_HC05.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=i19bluetoothhc05.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/i19bluetoothhc05.x.tar
