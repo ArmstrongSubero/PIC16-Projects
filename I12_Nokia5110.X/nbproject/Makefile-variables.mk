33af2e5a0186f497e5625d0fe67563444ebda689
Armstrong Subero <armstrongsubero@gmail.com>
2018-09-11 12:28:23 -0400

    Add files via upload

---
+++ I12_Nokia5110.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=I12_Nokia5110.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/I12_Nokia5110.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=i12nokia5110.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/i12nokia5110.x.tar
