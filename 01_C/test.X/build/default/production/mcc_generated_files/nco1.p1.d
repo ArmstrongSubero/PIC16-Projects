9a35be56609974fdab386fddc01fc1705c93394f
Armstrong Subero <armstrongsubero@gmail.com>
2019-04-22 14:58:41 -0400

    Update

--- 01_C/test.X/build/default/production/mcc_generated_files/nco1.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/mcc_generated_files/nco1.d  \
- build/default/production/mcc_generated_files/nco1.p1:  \
- mcc_generated_files/nco1.c  \
-mcc_generated_files/nco1.h 
--- 01_C/test.X/build/default/production/mcc_generated_files/pin_manager.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/mcc_generated_files/pin_manager.d  \
- build/default/production/mcc_generated_files/pin_manager.p1:  \
- mcc_generated_files/pin_manager.c  \
-mcc_generated_files/pin_manager.h 
