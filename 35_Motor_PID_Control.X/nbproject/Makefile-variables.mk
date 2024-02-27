143e11723c95f1adf4338f1f094abe0f9d249174
Armstrong Subero <armstrongsubero@gmail.com>
2024-02-26 23:59:38 -0400

    Initial Commit

---
+++ 35_Motor_PID_Control.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=35_Motor_PID_Control.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/35_Motor_PID_Control.X.production.hex
