f5f68da61d9ab375ae4cf4ba6228e65a27519ec2
Armstrong Subero <armstrongsubero@gmail.com>
2024-03-26 13:10:19 -0400

    Update

--- 43_SSD1306.X/nbproject/Makefile-variables.mk
+++ 43_SSD1306.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=IOC_tes.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/IOC_tes.X.production.hex
+CND_ARTIFACT_NAME_default=43_SSD1306.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/43_SSD1306.X.production.hex
